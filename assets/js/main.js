f47f5552211f824c2fa091e86fb9b4ffa07ade9c
danielryvero <danielryvero@gmail.com>
2024-02-24 00:21:06 +0100

    various changes with elizabeth

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
 		$wrapper = $('#wrapper'),
 		$header = $('#header'),
 		$nav = $('#nav'),
-		$main = $('#main'),
+		$main = $('#container_main'),
 		$navPanelToggle, $navPanel, $navPanelInner;
 
 	// Breakpoints.
